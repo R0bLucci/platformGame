8048fd1a1a892411781977690c15ae118f169225
robert <robyntimjnr@yahoo.it>
2017-06-09 01:31:58 +0100

    Initialize game's globals in one single place

---
+++ src/global.cpp
@@ -0,0 +1,10 @@
+namespace globals {
+	extern const int WIDTH = 640;
+	extern const int HEIGHT = 512; 
+	extern const int SPRITE_SCALER = 2;
+	extern const double WALK_SPEED = 0.2;
+
+	extern const double GRAVITY = 0.002;
+	extern const double JUMP = 0.7;
+	extern const double GRAVITY_CAP = 0.8;
+}
